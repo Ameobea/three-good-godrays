b2446b4f0520821332ffd6ce67fece0dee435ad2
Casey Primozic <me@ameo.link>
2024-08-30 00:00:29 -0700

    Disable logDepthBuffer in demo

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -10,7 +10,6 @@ window.addEventListener('load', () => {
   const renderer = new THREE.WebGLRenderer({
     powerPreference: 'high-performance',
     antialias: false,
-    logarithmicDepthBuffer: true,
   });
 
   const viewport = document.getElementById('viewport');
